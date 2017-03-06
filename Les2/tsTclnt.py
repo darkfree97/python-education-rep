1e980bf0d87f7b4b2d90f3388990f778861272e2
darkfree97 <darkfree97@gmail.com>
2017-03-06 18:20:57 +0200

    Add files via upload

---
+++ Les2/tsTclnt.py
@@ -0,0 +1,21 @@
+from socket import *
+
+HOST = "127.0.0.1"
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = bytes(input("> "), "utf-8")
+    if not data:
+        break
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode("utf-8"))
+
+tcpCliSock.close()
