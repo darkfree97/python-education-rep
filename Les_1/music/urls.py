b81accb4e5ce51e8443e11246dc171be7b23f865
darkfree97 <darkfree97@gmail.com>
2017-03-06 18:14:39 +0200

    Add files via upload

---
+++ Les_1/music/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+from . import views
+
+app_name = "music"
+
+urlpatterns = [
+    # # /music/
+    # url(r'^$', views.index, name="index"),
+    #
+    # # /music/album_id/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+    #
+    # # /music/album_id/favourite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite"),
+
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'register/$', views.UserFormView.as_view(), name="register"),
+    url(r'album/add/$', views.AlbumCreate.as_view(), name="album-add"),
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name="album-update"),
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name="album-delete"),
+
+]
